c30759bb973639d6e85ec24852f7826d2db3ed3a
petosorus <petosorus@hotmail.fr>
2022-10-06 02:20:59 +0200

    Adding scales for orbits and planet sizes

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn model(_app: &App) -> Model {
     Model {}
 }
 
-fn update(app: &App, _model: &mut Model, _update: Update) {
+fn update(_app: &App, _model: &mut Model, _update: Update) {
     
 }
 
@@ -23,16 +23,29 @@ fn view(app: &App, _model: &Model, frame: Frame){
 
     draw.background().color(PLUM);
 
+    // figure orbit scale out
+    let boundary = app.window_rect();
+    let height = boundary.top() - boundary.bottom();
+    let orbit_scale = (height / model::MAX_ORBIT) / 2.0;
+
+    // figure planet scale
+    let smallest_planet = model::SMALLEST_PLANET;
+    let smallest_drawing = 1.0;
+    let planet_scale = smallest_planet / smallest_drawing;
+
+    // sun
+    // draw.ellipse().color(YELLOW).radius(1392684.0 / planet_scale).x_y(0.0, 0.0);
     draw.ellipse().color(YELLOW).x_y(0.0, 0.0);
 
+    // draw the system
     let planets = &model::PLANETS;
     for planet in planets {
-        let time = (app.time / planet.year_in_days) * 100.0;
+        let time = (app.time / planet.year_in_days) * 1000.0;
 
-        let x = planet.orbit_radius * time.cos() + 0.0;
-        let y = planet.orbit_radius * time.sin() + 0.0;
+        let x = (planet.orbit_radius * orbit_scale) * time.cos();
+        let y = (planet.orbit_radius * orbit_scale) * time.sin();
 
-        draw.ellipse().color(planet.color).radius(planet.planet_radius).x_y(x, y);
+        draw.ellipse().color(planet.color).radius(planet.planet_radius / planet_scale).x_y(x, y);
     }
 
     draw.to_frame(app, &frame).unwrap();
--- src/model.rs
+++ src/model.rs
@@ -9,25 +9,64 @@ pub struct Planet {
     pub color: Srgb<u8>
 }
 
-pub static MERCURY: Planet = Planet {
-    orbit_radius: 80.0, // 57 909 050
-    planet_radius: 10.0, // 2 439,7
+static MERCURY: Planet = Planet {
+    orbit_radius: 57909050.0, // 57 909 050
+    planet_radius: 2439.7, // 2 439,7
     year_in_days: 87.969,
     color: GREY
 };
 
-pub static VENUS: Planet = Planet {
-    orbit_radius: 200.0, // 108 209 500
-    planet_radius: 25.0, // 6 051,8
+static VENUS: Planet = Planet {
+    orbit_radius: 108209500.0, // 108 209 500
+    planet_radius: 6051.8, // 6 051,8
     year_in_days: 224.667,
     color: BEIGE
 };
 
-pub static EARTH: Planet = Planet {
-    orbit_radius: 300.0, // 149 597 887
-    planet_radius: 26.0, // 6 378,137
+static EARTH: Planet = Planet {
+    orbit_radius: 149597887.0, // 149 597 887
+    planet_radius: 6378.137, // 6 378,137
     year_in_days: 365.256,
     color: STEELBLUE
 };
 
-pub static PLANETS: [Planet; 3] = [MERCURY, VENUS, EARTH];
+static MARS: Planet = Planet {
+    orbit_radius: 227944000.0, // 227 944 000
+    planet_radius: 3396.2, //3 396,2
+    year_in_days: 686.885,
+    color: BROWN
+};
+
+static JUPITER: Planet = Planet {
+    orbit_radius: 778340000.0, // 778 340 000
+    planet_radius: 71492.0, // 71 492 
+    year_in_days: 4332.01,
+    color: DARKSALMON
+};
+
+static SATURN: Planet = Planet {
+    orbit_radius: 1426700000.0, // 1 426 700 000
+    planet_radius: 60268.0, // 60 268
+    year_in_days:  10754.0,
+    color: KHAKI
+};
+
+static URANUS: Planet = Planet {
+    orbit_radius: 2870700000.0, // 2 870 700 000
+    planet_radius: 25559.0, // 25 559
+    year_in_days: 30698.0,
+    color: LIGHTCYAN
+};
+
+static NEPTUNE: Planet = Planet {
+    orbit_radius: 4498400000.0, // 4 498 400 000
+    planet_radius: 24764.0, // 24 764
+    year_in_days: 60216.8,
+    color: CORNFLOWERBLUE
+};
+
+
+
+pub static PLANETS: [Planet; 8] = [NEPTUNE, URANUS, SATURN, JUPITER, MARS, EARTH, VENUS, MERCURY];
+pub static MAX_ORBIT: f32 = PLANETS[0].orbit_radius;
+pub static SMALLEST_PLANET: f32 = MERCURY.planet_radius;
